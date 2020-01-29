33150910da7433922f0b582e638a8692d9028ecb
isp26 <isp26@drexel.edu>
2020-01-28 22:57:57 -0500

    Made example scene with idea of movement.

---
+++ Project02/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb.AddForce(movement * moveSpeed);
+    }
+}
